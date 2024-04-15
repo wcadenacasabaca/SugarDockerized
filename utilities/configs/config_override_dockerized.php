31a40cd1d46edbd6b25f2bed845af0a26f34949f
Rafael Fernandes <87442654+rfernandes-sugarcrm@users.noreply.github.com>
2024-04-15 11:46:56 -0400

    Update config_override_dockerized.php (#9)

--- utilities/configs/config_override_dockerized.php
+++ utilities/configs/config_override_dockerized.php
@@ -1,11 +1,13 @@
 $sugar_config['full_text_engine']['Elastic']['host'] = 'sugar-elasticsearch';
 $sugar_config['full_text_engine']['Elastic']['port'] = '9200';
+$sugar_config['full_text_engine']['Elastic']['curl'] = array();
 $sugar_config['dbconfig']['db_host_name'] = 'sugar-mysql';
 $sugar_config['dbconfig']['db_user_name'] = 'root';
 $sugar_config['dbconfig']['db_password'] = 'root';
 $sugar_config['dbconfig']['db_name'] = 'sugar';
 $sugar_config['dbconfig']['db_type'] = 'mysql';
 $sugar_config['dbconfig']['db_host_name'] = 'sugar-mysql';
+$sugar_config['db_manager'] = 'MysqliManager';
 $sugar_config['external_cache']['redis']['host'] = 'sugar-redis';
 $sugar_config['external_cache_disabled'] = 'false';
 $sugar_config['external_cache_disabled_redis'] = 'false';
